6a514dc11c56bfded1220ab6e61ea341a09db0d0
vishal lokhande <lvishal108@gmail.com>
2025-01-03 11:51:50 +0000

    add boostrp , navbar , news , newsitem jsx ,newsItem , card ,remove strict mode

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import NewsItem from "./components/NewsItem";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <News />
+      </div>
+    );
+  }
+}
+
+export default App;
